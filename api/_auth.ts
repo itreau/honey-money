ebe3874ac4808dffc60d01269d61a179d9379af3
trey <9271344+itreau@users.noreply.github.com>
2026-04-07 08:30:44 -0700

    More auth fixes

--- api/_auth.ts
+++ api/_auth.ts
@@ -1,8 +1,8 @@
 import type { VercelRequest, VercelResponse } from '@vercel/node';
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = process.env.SUPABASE_URL;
-const supabaseAnonKey = process.env.SUPABASE_ANON_KEY;
+const supabaseUrl = process.env.VITE_SUPABASE_URL;
+const supabaseAnonKey = process.env.VITE_SUPABASE_ANON_KEY;
 
 export type AuthHandler = (
   req: VercelRequest,
